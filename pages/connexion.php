dbb9e8279560f9524c8c4ed77e0fdb9e3330a52c
ZakZidane <44093265+ZakZidane@users.noreply.github.com>
2018-11-29 18:42:01 +0100

    Update connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,7 +1,7 @@
 <?php
 $user='root';
 $pass='';
-echo "test modif" ;
+
 
 $dsn=mysqli_init();mysqli_ssl_set($dsn, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($dsn, "translatorwizata-mysqldbserver.mysql.database.azure.com", "EEAS@translatorwizata-mysqldbserver", {}, {translationdb}, 3306);
 try {
