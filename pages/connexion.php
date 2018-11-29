1ea053b73e951588b0ad2f8c2b25d3aed23209c6
ZakZidane <44093265+ZakZidane@users.noreply.github.com>
2018-11-29 15:17:22 +0100

    Update connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -3,7 +3,7 @@ $user='root';
 $pass='';
 
 
-$dsn = 'mysql:host=localhost;dbname=translationdb';
+$dsn=mysqli_init();mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($dsn, "translatorwizata-mysqldbserver.mysql.database.azure.com", "EEAS@translatorwizata-mysqldbserver", {}, {translationdb}, 3306);
 try {
    $pdo = new PDO($dsn, $user, $pass);
 } catch (PDOException $e) {
