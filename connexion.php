ca05c54044050f467fc8c3192590f896c543f67e
ZakZidane <44093265+ZakZidane@users.noreply.github.com>
2018-11-29 15:16:25 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+$user='root';
+$pass='';
+
+
+$dsn=mysqli_init(); 
+mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($dsn, "translatorwizata-mysqldbserver.mysql.database.azure.com", "EEAS@translatorwizata-mysqldbserver", {}, {translationdb}, 3306);
+
+
+try {
+   $pdo = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+   die("Error ! : ".$e->getMessage());
+}
+?>
