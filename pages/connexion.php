2fe7d964f9ca90d0b775641bc88e29bc8ac476e4
ZakZidane <44093265+ZakZidane@users.noreply.github.com>
2018-12-13 11:11:10 +0100

    Update connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,9 +1,9 @@
 <?php
 //$user='EEAS@translatorwizata-mysqldbserver';
-$user='eeas@wizatatranslator-mysqldbserver';
+$user='eeas@wizatatranslator';
 $pass='Wizata987';
 //$server="translatorwizata-mysqldbserver.mysql.database.azure.com";
-$server="wizatatranslator-mysqldbserver.mysql.database.azure.com";
+$server="wizatatranslator.mysql.database.azure.com";
 $bdd="translationdb";
 $dsn = "mysql:host=".$server."; dbname=".$bdd;
 try {
